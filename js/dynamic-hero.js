61ec70181d11278eadf71f667f47e57cf596fda4
qoehliang <105919775+qoehliang@users.noreply.github.com>
2025-01-04 00:00:19 +1100

    dynamic-hero

---
+++ js/dynamic-hero.js
@@ -0,0 +1,14 @@
+let images = [
+    '../img/hero-min.jpg', 
+    '../img/hero-min2.jpg', 
+    '../img/hero-min3.jpg' // Add more image paths as needed
+  ];
+  let currentImage = 0;
+  const heroElement = document.querySelector('.hero');
+  
+  function changeBackgroundImage() {
+    heroElement.style.backgroundImage = `url(${images[currentImage]})`;
+    currentImage = (currentImage + 1) % images.length; // Loop through images
+  }
+  
+  setInterval(changeBackgroundImage, 5000); // Change every 5000 milliseconds (5 seconds)
